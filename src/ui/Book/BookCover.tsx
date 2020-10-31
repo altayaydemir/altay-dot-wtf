a81af2b28559dabec13d327a084cc80a7087bfcf
Altay Aydemir <altay.aydemir@icloud.com>
2020-10-31 13:53:22 +0100

    remove exhaustive calculation in bookCover

--- src/ui/Book/BookCover.tsx
+++ src/ui/Book/BookCover.tsx
@@ -12,13 +12,11 @@ const BORDER_WIDTH = 1
 
 const BookCover: React.FC<Props> = ({ bookMeta, width = MAX_WIDTH }) => (
   <Box
-    height={(width / (bookMeta.coverImage.aspectRatio + BORDER_WIDTH)) * 2}
     sx={{
       display: 'inline-block',
       borderWidth: BORDER_WIDTH,
       borderStyle: 'solid',
       borderColor: 'borderPrimary',
-      backgroundColor: 'backgroundSecondary',
     }}
   >
     <Image
