10dbcda7624e7d5d1dd9fe25d59c57db5bd7140f
Altay Aydemir <altay@aydemir.io>
2020-11-03 07:29:40 +0100

    selectively load md elements

--- src/ui/Markdown/index.tsx
+++ src/ui/Markdown/index.tsx
@@ -2,26 +2,27 @@ import ReactMarkdown from 'react-markdown'
 import gfm from 'remark-gfm'
 import dynamic from 'next/dynamic'
 
-const MDParagraph = dynamic(() => import('./MDParagraph'))
-const MDHeading = dynamic(() => import('./MDHeading'))
+import MDHeading from './MDHeading'
+import MDParagraph from './MDParagraph'
+import MDLink from './MDLink'
+
+const MDImage = dynamic(() => import('./MDImage'))
+const MDQuote = dynamic(() => import('./MDQuote'))
 const MDInlineCode = dynamic(() => import('./MDInlineCode'))
 const MDCodeBlock = dynamic(() => import('./MDCodeBlock'))
-const MDImage = dynamic(() => import('./MDImage'))
-const MDLink = dynamic(() => import('./MDLink'))
-const MDQuote = dynamic(() => import('./MDQuote'))
 
 const Markdown: React.FC<{ children: string }> = ({ children }) => (
   <ReactMarkdown
     plugins={[gfm]}
     escapeHtml={true}
     renderers={{
-      inlineCode: MDInlineCode,
-      code: MDCodeBlock,
-      link: MDLink,
-      image: MDImage,
       paragraph: MDParagraph,
       heading: MDHeading,
+      link: MDLink,
+      image: MDImage,
       blockquote: MDQuote,
+      inlineCode: MDInlineCode,
+      code: MDCodeBlock,
     }}
   >
     {children}
