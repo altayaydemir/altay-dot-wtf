49b8e6d9f0152f81c54408d5ca413ca1d9de34ce
Altay Aydemir <altay@aydemir.io>
2020-11-10 22:53:51 +0100

    update notes page description

--- src/pages/notes/index.tsx
+++ src/pages/notes/index.tsx
@@ -10,7 +10,7 @@ export const getStaticProps = getStaticPropsForContentList<Note>('note')
 
 const NotesPage: React.FC<InferGetStaticPropsType<typeof getStaticProps>> = ({ data }) => (
   <>
-    <PageHeader title={`notes`} description={`🤔`} />
+    <PageHeader title={`notes`} description={`random thoughts and references.`} />
 
     <Box m={3} />
 
