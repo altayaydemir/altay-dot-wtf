7f416d231d68fe317d010a6ec44c6b3248458d3c
Altay Aydemir <altay@aydemir.io>
2020-11-11 21:53:59 +0100

    add shopify

---
+++ data/notes/how-to-design-a-checkout-experience.md
@@ -0,0 +1,18 @@
+---
+title: How to design a great checkout experience
+date: '2020-11-11T20:33:52Z'
+tags:
+  - design
+---
+
+Just do whatever Shopify does.
+
+I'm astonished by the swiftness of their checkout experience.
+It is so effortless that makes me worry about something went wrong.
+
+Everything with one hand.\
+Didn't need to type anything. One click, a fingerprint scan, done.
+
+![Shopify two taps](/images/notes/shopify-checkout-gg.gif)
+
+It's as good as the _I don't need to take my wallet off_ solution for coffee shops mentioned in [The Best Interface Is No Interface](/books/the-best-interface-is-no-interface).
--- src/core/contexts/Theme.tsx
+++ src/core/contexts/Theme.tsx
@@ -4,12 +4,14 @@ import { ThemeProvider as EmotionThemeProvider } from 'emotion-theming'
 import useDarkMode from 'use-dark-mode'
 import { createTheme } from 'theme'
 import { createGlobalStyles } from 'theme/globalStyles'
+import { usePlausible } from 'next-plausible'
 
 export const ThemeProvider: React.FC = ({ children }) => {
   const { value: dark } = useDarkMode(false, { storageKey: undefined, onChange: () => null })
+  const plausible = usePlausible()
 
   useEffect(() => {
-    ;(window as any).plausible?.('theme', { props: { type: dark ? 'dark' : 'light' } })
+    plausible('theme', { props: { type: dark ? 'dark' : 'light' } })
   }, [dark])
 
   return (
