22eed3ade9382be58242a6dc216be7a420ca78eb
Altay Aydemir <altay@aydemir.io>
2020-11-11 11:12:45 +0100

    update event fn

--- src/core/contexts/Theme.tsx
+++ src/core/contexts/Theme.tsx
@@ -3,18 +3,13 @@ import { Global } from '@emotion/core'
 import useDarkMode from 'use-dark-mode'
 import { createTheme } from 'theme'
 import { createGlobalStyles } from 'theme/globalStyles'
-import { usePlausible } from 'next-plausible'
 import { useEffect } from 'react'
-
-type PlausibleEventProperties = { props: Record<string, unknown> }
-type PlausibleEventFunction = (eventName: string, properties?: PlausibleEventProperties) => void
 
 export const ThemeProvider: React.FC = ({ children }) => {
   const { value: dark } = useDarkMode(false, { storageKey: undefined, onChange: () => null })
-  const plausible = usePlausible() as PlausibleEventFunction
 
   useEffect(() => {
-    plausible('theme', { props: { type: dark ? 'dark' : 'light' } })
+    ;(window as any).plausible?.('theme', { props: { type: dark ? 'dark' : 'light' } })
   }, [dark])
 
   return (
