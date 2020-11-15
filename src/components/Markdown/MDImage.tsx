eb17beba7ec60869fd201b79362a3b172a3a73dd
Altay Aydemir <altay@aydemir.io>
2020-11-15 18:48:08 +0100

    fix mobile image size

--- src/components/Markdown/MDImage.tsx
+++ src/components/Markdown/MDImage.tsx
@@ -16,7 +16,13 @@ const MDImage: React.FC<Props> = ({ src, alt }) => {
   return (
     <Box my={3}>
       <Zoom zoomMargin={96} overlayBgColorEnd={theme.colors.imageZoomBackground}>
-        <Box sx={{ position: 'relative', width: 640, height: 400 }}>
+        <Box
+          sx={{
+            position: 'relative',
+            width: [320, 480, 640],
+            height: [200, 300, 400],
+          }}
+        >
           <Image alt={alt} src={src} layout="fill" className="image-markdown" />
         </Box>
       </Zoom>
